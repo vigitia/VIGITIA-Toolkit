eeb0e811ee12fe3b2fb82e0b26a2a69aa48c375d
Vitus <vitus.maierhoefer@gmail.com>
2019-10-08 15:55:02 +0200

    Commenting code and code cleanup

--- prototype/experiments/transformation_rgb_depth.py
+++ prototype/experiments/transformation_rgb_depth.py
@@ -10,6 +10,7 @@ from hand_tracker import HandTracker
 # Code for Hand Tracking and Models from https://github.com/metalwhale/hand_tracking
 
 # Current distance between camera and table in cm
+# TODO: Should be calculated automatically later and saved to config file
 DISTANCE_CAMERA_TABLE = 110  # cm
 
 # Camera Settings
@@ -71,7 +72,7 @@ CONNECTIONS = [
 ]
 
 FABRIC_PATTERN_ONE = np.array([[-77, -56], [-41, -56], [116, -63], [191, -17], [156, 23], [85, -19], [96, 94],
-                                [-83, 102], [-77, -11], [-174, 28], [-188, -24]])
+                               [-83, 102], [-77, -11], [-174, 28], [-188, -24]])
 
 # IDs of the used Aruco Marker IDs of the Demo application
 ARUCO_MARKER_SHIRT_S = 0
@@ -141,6 +142,7 @@ class TransformationRGBDepth:
         self.depth_scale = depth_sensor.get_depth_scale()
         depth_sensor.set_option(rs.option.laser_power, 360)
 
+        # TODO: Tweak camera settings
         depth_sensor.set_option(rs.option.depth_units, 0.0001)
 
         # Create an align object
@@ -164,6 +166,7 @@ class TransformationRGBDepth:
     def mouse_click(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             print((x, y))
+            # TODO: Implement calibration mode to save clicked corners to txt document
 
     def init_colorizer(self):
         self.colorizer = rs.colorizer()
@@ -178,19 +181,18 @@ class TransformationRGBDepth:
     def init_aruco_tracking(self):
         self.aruco_dictionary = aruco.Dictionary_get(aruco.DICT_4X4_100)
         self.aruco_detector_parameters = aruco.DetectorParameters_create()
-        self.aruco_detector_parameters.adaptiveThreshConstant = 10
+        self.aruco_detector_parameters.adaptiveThreshConstant = 10  # TODO: Tweak value
 
     # Streaming loop
     def loop(self):
         try:
             while True:
                 self.frame += 1
-                # Get frameset of color and depth
-                frames = self.pipeline.wait_for_frames()
+                frames = self.pipeline.wait_for_frames()  # Get frameset of color and depth
                 color_image, depth_colormap, aligned_depth_frame = self.align_frames(frames)
 
-                depth_data = aligned_depth_frame.get_data()
-                np_image = np.asanyarray(depth_data)
+                # depth_data = aligned_depth_frame.get_data()
+                # np_image = np.asanyarray(depth_data)
                 #distance = np_image[int(len(np_image) / 2)][int(len(np_image[0]) / 2)] * self.depth_scale * 100
                 #print(str("%.2f" % distance) + ' cm')
 
@@ -203,92 +205,22 @@ class TransformationRGBDepth:
                 #         if distance > 1.08:
                 #             color_image[i, j] = (0, 0, 0)
 
+                # Save last color frame to display it later
                 self.last_color_frame = color_image
 
-                # Hand detection
-                if self.hand_tracking_enabled and self.frame % 8 == 0:
-                    self.hand_points, _ = self.detector(cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB))
-                    if self.hand_points is not None:
-                        # The fingertip in the google mediapipe handtracking model has the ID 8.
-                        distance_camera_fingertip = int(aligned_depth_frame.get_distance(int(self.hand_points[8][0]), int(self.hand_points[8][1])) * 100)
-                        distance_fingertip_table = DISTANCE_CAMERA_TABLE - distance_camera_fingertip
-                        if distance_fingertip_table < 0:
-                            self.last_distance = 0
-                        else:
-                            self.last_distance = distance_fingertip_table
-                    else:
-                        self.last_distance = None
+                self.detect_hands(color_image, aligned_depth_frame)
 
+                # Available modes of demo application
                 if self.display_mode == "RGB":
-                    if not self.calibration_mode:
-                        color_image = self.add_hand_tracking_points(color_image, self.hand_points)
-                        # Perspective Transformation on images
-                        color_image = self.perspective_transformation(color_image)
-                        if self.outline_enabled:
-                            color_image = self.highlight_objects(color_image, False)
-                        if self.aruco_markers_enabled:
-                            color_image, angle, tracker_centroid = self.track_aruco_markers(color_image, color_image)
-
-                        # Canny Edge Detection
-                        # https://www.pyimagesearch.com/2014/04/21/building-pokedex-python-finding-game-boy-screen-step-4-6/
-                        # color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
-                        # color_image = cv2.bilateralFilter(color_image, 11, 17, 17)
-                        # color_image = cv2.Canny(color_image, 100, 200)
-
-                        #mask_img = self.fgbg.apply(color_image)
-                        #color_image = cv2.bitwise_and(color_image, color_image, mask=mask_img)
-
-                        # Add black border on top to fill the missing pixels from 2:1 (16:8) to 16:9 aspect ratio
-                        color_image = self.add_border(color_image)
-                    cv2.imshow('window', color_image)
+                    self.display_mode_rgb(color_image)
                 elif self.display_mode == "depth":
-                    # Add black border on top to fill the missing pixels from 2:1 (16:8) to 16:9 aspect ratio
-                    if not self.calibration_mode:
-                        depth_colormap = self.add_hand_tracking_points(depth_colormap, self.hand_points)
-                        # Perspective Transformation on images
-                        depth_colormap = self.perspective_transformation(depth_colormap)
-                        depth_colormap = self.add_border(depth_colormap)
-
-                    cv2.imshow('window', depth_colormap)
+                    self.display_mode_depth(depth_colormap)
                 elif self.display_mode == "off":
-                    black_image = np.zeros((color_image.shape[0], color_image.shape[1], 3), np.uint8)
-                    black_image = self.add_hand_tracking_points(black_image, self.hand_points)
-                    black_image = self.perspective_transformation(black_image)
-
-                    if self.outline_enabled:
-                        black_image = self.highlight_objects(self.perspective_transformation(color_image), True)
-                    if self.aruco_markers_enabled:
-                        black_image, angle, tracker_centroid = self.track_aruco_markers(black_image,
-                                                                                        self.perspective_transformation(color_image))
-
-                    black_image = self.add_border(black_image)
-                    if self.last_distance is not None:
-                        cv2.putText(img=black_image, text=str(self.last_distance) + " cm",
-                                    org=(int(color_image.shape[1]/6), int(color_image.shape[0]/4)),
-                                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(255, 255, 255))
-
-                    cv2.imshow('window', black_image)
+                    self.display_mode_black_background(color_image)
                 elif self.display_mode == 'memory':
-                    copy = self.stored_image.copy()
-
-                    # Invert image
-                    #copy = cv2.bitwise_not(copy)
-
-                    # Canny Edge Detection
-                    # https://www.pyimagesearch.com/2014/04/21/building-pokedex-python-finding-game-boy-screen-step-4-6/
-                    copy = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
-                    copy = cv2.bilateralFilter(copy, 11, 17, 17)
-                    copy = cv2.Canny(copy, 40, 40)
-
-                    copy = self.add_border(copy)
-                    cv2.imshow('window', copy)
+                    self.display_mode_memory()
                 elif self.display_mode == "pattern":
-                    color_image = self.perspective_transformation(color_image)
-                    black_image = np.zeros((color_image.shape[0], color_image.shape[1], 3), np.uint8)
-                    self.pattern_example(black_image, color_image)
-
-                    black_image = self.add_border(black_image)
-                    cv2.imshow('window', black_image)
+                    self.display_mode_pattern(color_image)
 
                 key = cv2.waitKey(1)
                 # Press esc or 'q' to close the image window
@@ -300,9 +232,103 @@ class TransformationRGBDepth:
         finally:
             self.pipeline.stop()
 
+    def detect_hands(self, color_image, aligned_depth_frame):
+        # Hand detection (current implementation is far from real time). To reduce lag, the detection only
+        # takes place only once in 8 frames
+        if self.hand_tracking_enabled and self.frame % 8 == 0:
+            self.hand_points, _ = self.detector(cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB))
+            if self.hand_points is not None:
+                # The fingertip in the google mediapipe handtracking model has the ID 8.
+                distance_camera_fingertip = int(aligned_depth_frame.get_distance(int(self.hand_points[8][0]),
+                                                                                 int(self.hand_points[8][1])) * 100)
+                distance_fingertip_table = DISTANCE_CAMERA_TABLE - distance_camera_fingertip
+                if distance_fingertip_table < 0:
+                    self.last_distance = 0
+                else:
+                    self.last_distance = distance_fingertip_table
+            else:
+                self.last_distance = None
+
+    def display_mode_rgb(self, color_image):
+        if not self.calibration_mode:
+            color_image = self.add_hand_tracking_points(color_image, self.hand_points)
+            # Perspective Transformation on images
+            color_image = self.perspective_transformation(color_image)
+            if self.outline_enabled:
+                color_image = self.highlight_objects(color_image, False)
+            if self.aruco_markers_enabled:
+                color_image, angle, tracker_centroid = self.track_aruco_markers(color_image, color_image)
+
+            # Canny Edge Detection
+            # https://www.pyimagesearch.com/2014/04/21/building-pokedex-python-finding-game-boy-screen-step-4-6/
+            # color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+            # color_image = cv2.bilateralFilter(color_image, 11, 17, 17)
+            # color_image = cv2.Canny(color_image, 100, 200)
+
+            # mask_img = self.fgbg.apply(color_image)
+            # color_image = cv2.bitwise_and(color_image, color_image, mask=mask_img)
+
+            # Add black border on top to fill the missing pixels from 2:1 (16:8) to 16:9 aspect ratio
+            color_image = self.add_border(color_image)
+        cv2.imshow('window', color_image)
+
+    def display_mode_depth(self, depth_colormap):
+        if not self.calibration_mode:
+            depth_colormap = self.add_hand_tracking_points(depth_colormap, self.hand_points)
+            # Perspective Transformation on images
+            depth_colormap = self.perspective_transformation(depth_colormap)
+            depth_colormap = self.add_border(depth_colormap)
+
+        cv2.imshow('window', depth_colormap)
+
+    def display_mode_black_background(self, color_image):
+        black_image = np.zeros((color_image.shape[0], color_image.shape[1], 3), np.uint8)
+        black_image = self.add_hand_tracking_points(black_image, self.hand_points)
+        black_image = self.perspective_transformation(black_image)
+
+        if self.outline_enabled:
+            black_image = self.highlight_objects(self.perspective_transformation(color_image), True)
+        if self.aruco_markers_enabled:
+            black_image, angle, tracker_centroid = self.track_aruco_markers(black_image,
+                                                                            self.perspective_transformation(
+                                                                                color_image))
+
+        black_image = self.add_border(black_image)
+        if self.last_distance is not None:
+            cv2.putText(img=black_image, text=str(self.last_distance) + " cm",
+                        org=(int(color_image.shape[1] / 6), int(color_image.shape[0] / 4)),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(255, 255, 255))
+
+        cv2.imshow('window', black_image)
+
+    def display_mode_memory(self):
+        copy = self.stored_image.copy()
+
+        # Invert image
+        # copy = cv2.bitwise_not(copy)
+
+        # Canny Edge Detection
+        # https://www.pyimagesearch.com/2014/04/21/building-pokedex-python-finding-game-boy-screen-step-4-6/
+        copy = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+        copy = cv2.bilateralFilter(copy, 11, 17, 17)
+        copy = cv2.Canny(copy, 40, 40)
+
+        copy = self.add_border(copy)
+        cv2.imshow('window', copy)
+
+    def display_mode_pattern(self, color_image):
+        color_image = self.perspective_transformation(color_image)
+        black_image = np.zeros((color_image.shape[0], color_image.shape[1], 3), np.uint8)
+        self.pattern_example(black_image, color_image)
+
+        black_image = self.add_border(black_image)
+        cv2.imshow('window', black_image)
+
     # Put a white circle around all objects on the table, like a spotlight
     def highlight_objects(self, frame, draw_on_black=True):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # TODO: Tweak values and implement cutoff and moving average filter
         frame = cv2.bilateralFilter(frame, 11, 17, 17)
         ret, thresh = cv2.threshold(frame, 100, 255, 0)  # Define treshold here. Still needs tweaking
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -318,31 +344,25 @@ class TransformationRGBDepth:
 
         return frame
 
+    # Rotate a list of points around their centroid
+    def rotate_points(self, points, angle):
+        rotation_matrix = self.get_rotation_matrix(angle)
+        centroid = self.centroid(points)  # Get centroid
+        points = points - centroid  # Move points to origin for rotation
+        #print(points.astype(int).tolist())
+
+        # Rotate (see: https://scipython.com/book/chapter-6-numpy/examples/creating-a-rotation-matrix-in-numpy/)
+        points = np.dot(points, rotation_matrix.T)
+        points = points + centroid  # Move points back to original position
+        points = points.astype(int)  # Convert all floats to int because coordinates are expected to be Integers
+        return points
+
     # https://scipython.com/book/chapter-6-numpy/examples/creating-a-rotation-matrix-in-numpy/
     def get_rotation_matrix(self, angle):
         theta = np.radians(angle)
         c, s = np.cos(theta), np.sin(theta)
         rotation_matrix = np.array(((c, -s), (s, c)))
         return rotation_matrix
-
-    def rotate_points(self, points, angle):
-        rotation_matrix = self.get_rotation_matrix(angle)
-        # Get centroid
-        centroid = self.centroid(points)
-
-        # Move to origin
-        points = points - centroid
-        #print(points.astype(int).tolist())
-
-        # Rotate (see: https://scipython.com/book/chapter-6-numpy/examples/creating-a-rotation-matrix-in-numpy/)
-        points = np.dot(points, rotation_matrix.T)
-
-        # Move back to original position
-        points = points + centroid
-
-        # Convert all floats to int
-        points = points.astype(int)
-        return points
 
     # https://progr.interplanety.org/en/python-how-to-find-the-polygon-center-coordinates/
     def centroid(self, vertexes):
@@ -354,19 +374,15 @@ class TransformationRGBDepth:
         return (_x, _y)
 
     # Code for tracking Aruco markers taken from https://github.com/njanirudh/Aruco_Tracker
-    def track_aruco_markers(self, frame, frame_color):
+    def track_aruco_markers(self, frame, frame_color, draw_detected_markers=False):
         gray = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY)
         corners, ids, rejected_points = aruco.detectMarkers(gray, self.aruco_dictionary,
                                                             parameters=self.aruco_detector_parameters)
 
         aruco_markers = {}
 
-        # angle = 0
-        # tracker_centroid = None
-
         # check if the ids list is not empty
         if np.all(ids is not None):
-            print(ids)
             for i in range(len(ids)):
 
                 aruco_marker = {'angle': self.calculate_aruco_marker_rotation(corners[i][0], frame),
@@ -375,23 +391,22 @@ class TransformationRGBDepth:
 
                 aruco_markers[ids[i][0]] = aruco_marker
 
+                # angle = self.calculate_aruco_marker_rotation(corners[i][0], frame)
+                # tracker_centroid = self.centroid(corners[i][0])
+                # tracker_relative_x, tracker_relative_y = self.calculate_aruco_marker_relative_pos(tracker_centroid, frame)
 
-                #if ids[i] == 4:
-                    # angle = self.calculate_aruco_marker_rotation(corners[i][0], frame)
-                    # tracker_centroid = self.centroid(corners[i][0])
-                    # tracker_relative_x, tracker_relative_y = self.calculate_aruco_marker_relative_pos(tracker_centroid, frame)
+                # cv2.circle(frame, (int(tracker_centroid[0]), int(tracker_centroid[1])), 5, (0, 0, 255), -1)
 
-                    # cv2.circle(frame, (int(tracker_centroid[0]), int(tracker_centroid[1])), 5, (0, 0, 255), -1)
+                # Display info. ONLY FOR TESTING PURPOSES
+                # cv2.putText(img=frame, text=str(int(angle)) + ' Grad' +
+                #             ' Rel X: ' + str(int(tracker_relative_x * 100)) + '%' +
+                #             ' Rel Y: ' + str(int(tracker_relative_y * 100)) + '%',
+                #             org=(int(frame.shape[1] / 6), int(frame.shape[0] / 4)),
+                #             fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(255, 255, 255))
 
-                    # Display info. ONLY FOR TESTING PURPOSES
-                    # cv2.putText(img=frame, text=str(int(angle)) + ' Grad' +
-                    #             ' Rel X: ' + str(int(tracker_relative_x * 100)) + '%' +
-                    #             ' Rel Y: ' + str(int(tracker_relative_y * 100)) + '%',
-                    #             org=(int(frame.shape[1] / 6), int(frame.shape[0] / 4)),
-                    #             fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(255, 255, 255))
-
-            # draw a square around the markers
-            #aruco.drawDetectedMarkers(frame, corners, ids)
+            if draw_detected_markers:
+                # draw a square around the markers
+                aruco.drawDetectedMarkers(frame, corners, ids)
 
         print(map(lambda aruco_marker: aruco_marker['id'], aruco_markers))
 
@@ -415,6 +430,7 @@ class TransformationRGBDepth:
         tracker_relative_y = (tracker_centroid[1] / frame.shape[0])
         return tracker_relative_x, tracker_relative_y
 
+    # Calculate the angle between the two given vectors
     def calculate_angle(self, v1, v2):
         # https://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional-vectors-in-python
         angle = np.math.atan2(np.linalg.det([v1, v2]), np.dot(v1, v2))
