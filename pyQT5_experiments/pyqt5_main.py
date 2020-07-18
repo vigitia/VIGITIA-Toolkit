af227e46eedf14ae683cc5958a6f0b5069dea792
Vitus <vitus.maierhoefer@gmail.com>
2020-07-18 20:21:16 +0200

    Add dynamic loading of applications to the Rendering manager

--- pyQT5_experiments/pyqt5_main.py
+++ pyQT5_experiments/pyqt5_main.py
@@ -3,10 +3,18 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 import sys
+import os
+from os import listdir
+from pathlib import Path
+from os.path import isfile, join
+from importlib import import_module
+import pyclbr
 
-from pyQT5_experiments.qt_custom_transparent_widget_example import TransparentWidget
-from pyQT5_experiments.pyqt_paint_example import PaintExample
+#from pyQT5_experiments.qt_custom_transparent_widget_example import TransparentWidget
+#from pyQT5_experiments.pyqt_paint_example import PaintExample
 from pyQT5_experiments.pyQT5_browser import BrowserWidget
+
+APPLICATIONS_BASE_FOLDER = 'applications'
 
 class Window(QMainWindow):
     def __init__(self):
@@ -36,6 +44,7 @@ class Window(QMainWindow):
         # web2.load(QUrl('https://google.com'))
         # web2.setGeometry(1000, 1000, 300, 300)
 
+
         web = QWebEngineView()
         web.setMaximumWidth(1000)
         web.load(QUrl('https://maps.google.com'))
@@ -51,28 +60,50 @@ class Window(QMainWindow):
 
         proxy.setRotation(45)
 
-        paint = PaintExample()
+        #paint = PaintExample()
 
 
-        transparent = TransparentWidget()
-        transparent.setMaximumWidth(1000)
+        #transparent = TransparentWidget()
+        #transparent.setMaximumWidth(1000)
+
 
         # Use a 1x1 Grid layout to allow free placement of the widgets
         layout = QGridLayout()
-        #layout.addWidget(web, 0, 0, Qt.AlignLeft)
-        #layout.addWidget(web2, 0, 0, Qt.AlignLeft)
-        #layout.addWidget(transparent, 0, 0, Qt.AlignLeft)
-        layout.addWidget(paint, 0, 0, Qt.AlignRight)
-        layout.addWidget(graphics_view, 0, 0, Qt.AlignLeft)
 
+        applications = self.find_available_applications()
+        for application in applications:
+            layout.addWidget(application, 0, 0, Qt.AlignRight)
+
+        layout.addWidget(graphics_view, 0, 0, Qt.AlignLeft)
 
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
+    def find_available_applications(self):
+        applications = []
 
+        applications_path = os.path.join(Path(__file__).resolve().parent.parent, APPLICATIONS_BASE_FOLDER)
 
-        #self.setCentralWidget(web)
+        # Inspired by https://stackoverflow.com/questions/1057431/how-to-load-all-modules-in-a-folder
+        files = [f for f in listdir(applications_path) if isfile(join(applications_path, f)) and f != '__init__.py']
+        for file in files:
+            module_name = f"{'applications'}.{file[:-3]}"
+            module_info = pyclbr.readmodule(module_name)
+            module = import_module(module_name)
+
+            for item in module_info.values():
+                class_name = item.name
+                my_class = getattr(module, class_name)
+                superclasses = my_class.mro()
+                for superclass in superclasses:
+                    if superclass.__name__ == 'VIGITIAApplication':
+                        print(class_name, '{} in Module {} is a VIGITIA Application'.format(class_name, module_name))
+                        application = my_class()
+
+                        applications.append(application)
+
+        return applications
 
 
     def keyPressEvent(self, event):
