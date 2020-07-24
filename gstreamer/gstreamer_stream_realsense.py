9677bf24e399f0ed66cbe5e58a0524723d0caaeb
Vitus <vitus.maierhoefer@gmail.com>
2020-07-25 00:01:04 +0200

    Create a class for streaming frames

--- gstreamer/gstreamer_stream_realsense.py
+++ gstreamer/gstreamer_stream_realsense.py
@@ -6,29 +6,22 @@ import cv2
 from sensors.cameras.realsenseD435.realsense_D435_camera import RealsenseD435Camera
 
 
-def send():
-    camera = RealsenseD435Camera()
-    camera.start()
+class VIGITIAVideoStreamer:
 
-    out_send = cv2.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5000',cv2.CAP_GSTREAMER, 0, 30, (1280, 720), True)
+    def __init__(self):
 
-    if not out_send.isOpened():
-        print('VideoCapture or VideoWriter not opened')
-        exit(0)
+        host_ip = '132.199.130.68'
+        port = 5000
 
-    while True:
-        color_image, depth_image = camera.get_frames()
+        pipeline = 'appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host={} port={}'.format(host_ip, port)
+        fps = 30
+        res_x = 1280
+        res_y = 720
 
-        if color_image is None:
-            print('empty frame')
+        self.video_writer = cv2.VideoWriter(pipeline, cv2.CAP_GSTREAMER, fourcc=0, fps=fps, frameSize=(res_x, res_y), isColor=True)
+
+    def stream_frame(self, frame):
+        if not self.video_writer.isOpened():
+            print('VideoWriter to opened')
         else:
-            out_send.write(color_image)
-
-            cv2.imshow('send', color_image)
-        if cv2.waitKey(1)&0xFF == ord('q'):
-            break
-
-    out_send.release()
-
-
-send()
+            self.video_writer.write(frame)
