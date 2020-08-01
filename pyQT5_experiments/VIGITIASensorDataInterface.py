c0d86ef31358246931e2b1e90abfe6815a88e7ce
Vitus <vitus.maierhoefer@gmail.com>
2020-08-01 20:11:49 +0200

    Add forwarding of video streams to SensorDataInterface

--- pyQT5_experiments/VIGITIASensorDataInterface.py
+++ pyQT5_experiments/VIGITIASensorDataInterface.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# The TUIO 2.0 client decodes recieved TUIO messages
-# using the python-osc library: https://github.com/attwad/python-osc
+# This class functions as a TUIO 2.0 client and decodes recieved TUIO messages using the python-osc library:
+# https://github.com/attwad/python-osc
 
-# Based upon the TUIO 2.0 C++ Library by Martin Kaltebrunner
+
+# Code parts for the TUIO client based on the TUIO 2.0 Protocol Specification (http://www.tuio.org/?tuio20) and the
+# TUIO 2.0 C++ Library by Martin Kaltebrunner:
 # https://github.com/mkalten/TUIO20_CPP/blob/b3fc7998670200091e5768747c3e04ac758084e3/TUIO2/TuioServer.cpp
-# and the TUIO 2.0 Protocol Specification by Martin Kaltenbrunner
-# http://www.tuio.org/?tuio20
-
-# Using the Observer Pattern
-# Using the Singleton Pattern ()
-
 
 import sys
 import threading
 
+from gstreamer.VIGITIAVideoStreamReceiver import VIGITIAVideoStreamReceiver
 from utility.get_ip import get_ip_address
 
 from pythonosc.osc_server import ThreadingOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 
+# Port where this application will listen for incoming TUIO messages
 PORT = 8000
 
 
@@ -52,6 +50,7 @@ class VIGITIASensorDataInterface:
         # IP needs to be always the IP of the computer
         self.ip = get_ip_address()
 
+        # Using the Observer pattern
         self.subscribers = set()
 
         self.tokens = []
@@ -61,6 +60,7 @@ class VIGITIASensorDataInterface:
         self.symbols = []
 
         self.init_tuio_interface()
+        self.init_video_stream_receivers()
 
     def register_subscriber(self, new_subscriber):
         print('New Subscriber:', new_subscriber.__class__.__name__)
@@ -79,12 +79,26 @@ class VIGITIASensorDataInterface:
 
         osc_udp_server = ThreadingOSCUDPServer((self.ip, PORT), dispatcher)
 
-        print("Listening on {}".format(osc_udp_server.server_address))
+        print("Listening on {} for incoming TUIO messages".format(osc_udp_server.server_address))
 
         server_thread = threading.Thread(target=osc_udp_server.serve_forever)
         server_thread.start()
 
-        print('Initialized TUIO interface')
+    #
+    def init_video_stream_receivers(self):
+
+        ports = [5000]
+
+        for port in ports:
+            receiver = VIGITIAVideoStreamReceiver('RealSense D435 RGB', port=port)
+            receiver.register_subscriber(self)
+            receiver.start()
+
+    def on_new_video_frame(self, frame, name):
+        print('New frame received of type', name)
+
+        for subscriber in self.subscribers:
+            subscriber.on_new_video_frame(frame, name)
 
     def on_new_frame_message(self, *message):
         #print('New frame arrived:', message)
