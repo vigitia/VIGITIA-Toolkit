daff14492bc842083c38fceaaa85fd7be8f17f47
Vitus <vitus.maierhoefer@gmail.com>
2020-08-02 15:17:23 +0200

    Test movement of application

--- applications/ImageWidget.py
+++ applications/ImageWidget.py
@@ -2,22 +2,45 @@
 # Based on https://pythonspot.com/pyqt5-image/
 import os
 import sys
+from time import sleep
+
+from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QIcon, QPixmap
 
 from apps.VIGITIABaseApplication import VIGITIABaseApplication
 
 
+class Thread(QThread):
+
+    def __init__(self, widget):
+        QThread.__init__(self)
+        self.widget = widget
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        for i in range(1000):
+            sleep(0.5)
+            self.widget.set_x(self.widget.get_x() + i)
+            print('Move widget to {}'.format(self.widget.get_x()))
+
 class ImageWidget(QWidget, VIGITIABaseApplication):
 
-    def __init__(self):
+    def __init__(self, rendering_manager):
         super().__init__()
+        self.set_name(self.__class__.__name__)
+        self.set_rendering_manager(rendering_manager)
         self.x = 300
         self.y = 200
         self.width = 800
         self.height = 600
         self.rotation = 45
         self.initUI()
+
+        self.myThread = Thread(self)
+        self.myThread.start()
 
     def initUI(self):
         self.setGeometry(0, 0, self.get_width(), self.get_height())
