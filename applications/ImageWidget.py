bbca98c8dc02bb5a09a180b47a58cd8f442139fe
Vitus <vitus.maierhoefer@gmail.com>
2020-08-05 11:18:18 +0200

    Hide an image if a corresponding token is missing

--- applications/ImageWidget.py
+++ applications/ImageWidget.py
@@ -1,13 +1,10 @@
-
-# Based on https://pythonspot.com/pyqt5-image/
 import math
 import os
-import sys
-from time import sleep
+import time
 
 from PyQt5.QtCore import QThread, Qt, QPoint
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QIcon, QPixmap, QTransform
+from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtGui import QPixmap, QTransform
 
 from apps.VIGITIABaseApplication import VIGITIABaseApplication
 
@@ -22,26 +19,23 @@ class Thread(QThread):
         self.wait()
 
     def run(self):
-        pass
-        # for i in range(1000):
-        #     sleep(1)
-        #     self.widget.set_rotation(self.widget.get_rotation() + 1)
-        #     self.widget.set_x(self.widget.get_x() + i)
-        #     #self.widget.set_width(self.widget.get_width() - 25)
-        #     print('Move widget to {}'.format(self.widget.get_x()))
+        while True:
+            TIME_FOR_IMAGE_TO_DISAPPEAR = 2  # sec
+            if time.time() - self.widget.last_time_token_seen > TIME_FOR_IMAGE_TO_DISAPPEAR and self.widget.image_visible:
+                self.widget.image_visible = False
+                self.widget.label.hide()
 
 
 class ImageWidget(QWidget, VIGITIABaseApplication):
 
     image_rotation = 0
+    last_time_token_seen = 0
+    image_visible = False
 
     def __init__(self, rendering_manager):
         super().__init__()
         self.set_name(self.__class__.__name__)
         self.set_rendering_manager(rendering_manager)
-        # self.set_position(1000, 500)
-        # self.set_dimensions(800, 600)
-        # self.set_rotation(45)
         self.initUI()
 
         self.myThread = Thread(self)
@@ -51,16 +45,14 @@ class ImageWidget(QWidget, VIGITIABaseApplication):
         self.setGeometry(0, 0, self.get_width(), self.get_height())
         self.setStyleSheet("background-color: transparent;")
 
+        # How to display an image based on https://pythonspot.com/pyqt5-image/
         self.label = QLabel(self)
         self.label.hide()
-        #self.label.resize(self.get_width(), self.get_height())
         self.label.setAlignment(Qt.AlignCenter)
         pixmap = QPixmap(os.path.abspath(os.path.join(os.path.dirname(__file__), 'image.jpg')))
         image_diagonal = self.get_diagonal(pixmap.width(), pixmap.height())
         self.label.resize(image_diagonal, image_diagonal)
         self.label.setPixmap(pixmap)
-
-        # self.resize(pixmap.width(), pixmap.height())
 
     def get_diagonal(self, width, height):
         diagonal = math.ceil(math.sqrt(width * width + height * height))
@@ -70,6 +62,8 @@ class ImageWidget(QWidget, VIGITIABaseApplication):
         # print('Tokens:', data)
         for token in data:
             if token['component_id'] == 40:
+                self.last_time_token_seen = time.time()
+                self.image_visible = True
                 self.label.show()
                 token_angle = token['angle']
                 if self.image_rotation not in range(token_angle - 1, token_angle + 1):
@@ -78,7 +72,6 @@ class ImageWidget(QWidget, VIGITIABaseApplication):
                     transform = QTransform().rotate(token_angle)
                     self.image_rotation = token_angle
                     pixmap = pixmap.transformed(transform, Qt.SmoothTransformation)
-
 
                     self.label.setPixmap(pixmap)
 
@@ -89,7 +82,3 @@ class ImageWidget(QWidget, VIGITIABaseApplication):
                     self.label.move(local_pos.x() - self.label.width()/2, local_pos.y() - self.label.height()/2 + 500)
                 except Exception as e:
                     print(e)
-
-            if token['component_id'] != 40:
-                self.label.hide()
-
