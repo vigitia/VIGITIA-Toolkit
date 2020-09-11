e6d6dea8ed6ca82b75808d2e316cccccba11493e
Vitus <vitus.maierhoefer@gmail.com>
2020-09-12 00:04:39 +0200

    Rework object detector to extract regions before detection

--- services/generic_object_detector.py
+++ services/generic_object_detector.py
@@ -6,6 +6,8 @@ from cvlib.object_detection import draw_bbox
 
 import numpy as np
 
+DEBUG_MODE = True
+
 
 class GenericObjectDetector:
 
@@ -14,42 +16,77 @@ class GenericObjectDetector:
 
     def detect_generic_objects(self, frame, mask):
 
-        self.extract_objects(frame, mask)
+        detected_objects = []
+
+        extracted_objects = self.extract_objects(frame, mask)
+
+        for i, extracted_object in enumerate(extracted_objects):
+            if i > 9:
+                break
+            label, confidence = self.detect_extracted_object(extracted_object['extracted_object'])
+            print(label)
+            cv2.rectangle(frame, (extracted_object['x'], extracted_object['y']), (extracted_object['width'], extracted_object['height']), (0, 0, 255), 2)
+
+            cv2.putText(img=frame, text=str(label), org=(int(extracted_object['x']), int(extracted_object['y'])),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(0, 0, 0))
+
+
+        cv2.imshow('labels', frame)
+
+
+        # print(frame.shape)
+        #
+        # # bbox, label, conf = detect_common_objects(frame, confidence=0.25)
+        # # bbox, label, conf = detect_common_objects(frame, confidence=0.75, enable_gpu=True)
+        # bbox, label, conf = detect_common_objects(frame, confidence=0.3, model='yolov3-tiny')
+        #
+        # if DEBUG_MODE:
+        #     output_image = draw_bbox(frame, bbox, label, conf)
+        #
+        # for i in range(len(bbox)):
+        #     detected_objects.append({
+        #         'label': label[i],
+        #         'bbox': bbox[i],
+        #         'conf': conf[i]
+        #     })
+        #
+        # print(detected_objects)
+
+        return detected_objects
+
+    def detect_extracted_object(self, extracted_object):
+
+        label = ''
+        confidence = 0
 
         # bbox, label, conf = detect_common_objects(frame, confidence=0.25)
         # bbox, label, conf = detect_common_objects(frame, confidence=0.75, enable_gpu=True)
-        bbox, label, conf = detect_common_objects(frame, confidence=0.3, model='yolov3-tiny')
+        bbox, label, conf = detect_common_objects(extracted_object, confidence=0.3, model='yolov3-tiny')
 
-        output_image = draw_bbox(frame, bbox, label, conf)
+        if len(label) > 0:
+            label = label
+            confidence = conf[0]
 
-        detected_objects = []
-
-        for i in range(len(bbox)):
-            detected_objects.append({
-                'label': label[i],
-                'bbox': bbox[i],
-                'conf': conf[i]
-            })
-
-        print(detected_objects)
-
-        return output_image, detected_objects
+        return label, confidence
 
     def extract_objects(self, frame, mask):
+
+        kernel = np.ones((5, 5), np.uint8)
+        frame = cv2.erode(frame, kernel, iterations=2)
+        frame = cv2.dilate(frame, kernel, iterations=2)
+
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         contours_filtered = []
-        hierarchy_filtered = []
+
+        MIN_CONTOUR_SIZE = 100
 
         for i, contour in enumerate(contours):
             if hierarchy[0, i, 3] <= 1:
-                print(hierarchy[0, i, 3])
+                #print(hierarchy[0, i, 3])
                 area = cv2.contourArea(contour)
-                if area > 100:
+                if area > MIN_CONTOUR_SIZE:
                     contours_filtered.append(contour)
-                    # hierarchy_filtered.append(hierarchy[0,i,3])
-
-        print(hierarchy_filtered)
 
         # https://docs.opencv.org/3.4/da/d0c/tutorial_bounding_rects_circles.html
         contours_poly = [None] * len(contours)
@@ -58,9 +95,8 @@ class GenericObjectDetector:
             contours_poly[i] = cv2.approxPolyDP(c, 3, True)
             boundRect[i] = cv2.boundingRect(contours_poly[i])
 
-        drawing = frame.copy()
-
-        EXPAND_BOX = 50
+        EXPAND_BOX = 20
+        MIN_LENGTH = 50
 
         objects_extracted = []
 
@@ -71,8 +107,18 @@ class GenericObjectDetector:
             width = int(boundRect[i][0] + boundRect[i][2] + EXPAND_BOX)
             height = int(boundRect[i][1] + boundRect[i][3]) + EXPAND_BOX
 
-            cv2.rectangle(drawing, (x, y), (width, height), color, 2)
-            object = frame.copy()[y:y + height, x:x + width]
-            objects_extracted.append(object)
+            extracted_object = frame.copy()[y:y + height, x:x + width]
 
-        cv2.imshow('Contours', drawing)
+            if extracted_object.shape[0] > MIN_LENGTH and extracted_object.shape[1] > MIN_LENGTH:
+
+                object_info = {
+                    'x': x,
+                    'y': y,
+                    'width': width,
+                    'height': height,
+                    'extracted_object': extracted_object
+                }
+
+                objects_extracted.append(object_info)
+
+        return objects_extracted
