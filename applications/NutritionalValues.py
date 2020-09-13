643b023dcc9841eb1e41da21ecaef13336004a38
Vitus <vitus.maierhoefer@gmail.com>
2020-09-13 13:20:19 +0200

    Add smoothing for coordinates

--- applications/NutritionalValues.py
+++ applications/NutritionalValues.py
@@ -3,7 +3,7 @@ import time
 
 from PyQt5 import uic
 from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtGui import QImage, QPainter, QPen, QBrush
+from PyQt5.QtGui import QImage, QPainter, QPen, QBrush, QColor
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
 
 from apps.VIGITIABaseApplication import VIGITIABaseApplication
@@ -29,9 +29,9 @@ class NutritionalValues(QWidget, VIGITIABaseApplication):
 
         self.nutri_scores = []
 
-        self.nutri_score_banana = NutriScore(self, 0, 0, 'Banane', '115', '26,4g', '1,2g', '0,2g')
-        self.nutri_score_orange = NutriScore(self, 0, 0, 'Orange', '115', '26,4g', '1,2g', '0,2g')
-        self.nutri_score_carrot = NutriScore(self, 0, 0, 'Karotte', '115', '26,4g', '1,2g', '0,2g')
+        self.nutri_score_banana = NutriScore(self, 0, 0, 'Banane', '115kcal', '26,4g', '1,2g', '0,2g')
+        self.nutri_score_orange = NutriScore(self, 0, 0, 'Orange', '56kcal', '10,7g', '1,3g', '0,3g')
+        self.nutri_score_carrot = NutriScore(self, 0, 0, 'Karotte', '32kcal', '7,2g', '0,8g', '0,0g')
         self.nutri_score_banana.hide()
         self.nutri_score_orange.hide()
         self.nutri_score_carrot.hide()
@@ -77,10 +77,11 @@ class NutritionalValues(QWidget, VIGITIABaseApplication):
         self.image = QImage(self.size(), QImage.Format_ARGB32)
         self.image.fill(Qt.transparent)
 
-    def draw_circle(self, x, y, width, height, color):
+    def draw_circle(self, x, y, width, height):
         painter = QPainter(self.image)
-        painter.setPen(QPen(color, 10, Qt.SolidLine))
-        painter.drawRect(x, y, width, height)
+        painter.setPen(QPen(QColor(255, 255, 255, 150), 10, Qt.SolidLine))
+        painter.drawEllipse(x, y, width, height)
+        #painter.drawLine(int(x + width/2), int(y + height/2), x + width, 200)
 
     # paint event
     def paintEvent(self, event):
@@ -94,68 +95,45 @@ class NutritionalValues(QWidget, VIGITIABaseApplication):
         self.moveToThread(self.rendering_manager)
         NutriScore(self, 100, 100)
 
-    # def on_new_token_messages(self, data):
-    #     # print('Tokens:', data)
-    #     for token in data:
-    #         if token['component_id'] == 1000:
-    #             print(token)
-    #             self.nutri_score_orange.show()
-    #             self.nutri_score_orange.move(token['x_pos'], token['y_pos'])
-    #         elif token['component_id'] == 37:
-    #             self.nutri_score_carrot.show()
-    #             self.nutri_score_carrot.move(token['x_pos'], token['y_pos'])
-    #         elif token['component_id'] == 44:
-    #             self.nutri_score_banana.show()
-    #             self.nutri_score_banana.move(token['x_pos'], token['y_pos'])
-
-                #if token['component_id'] not in self.present_tokens:
-                    # self.add_widget()
-                    #
-                    # token_info = {
-                    #     'id': token['component_id'],
-                    #     'name': 'Banana',
-                    #     'x': token['x_pos'],
-                    #     'y': token['y_pos'],
-                    #     'last_time_seen': time.time(),
-                    #     'info_widget': ''
-                    # }
-                    # self.present_tokens.append(token['component_id'])
-
-    # def on_new_bounding_box_messages(self, data):
-    #     self.reset_image()
-    #
-    #     for bounding_box in data:
-    #         self.draw_circle(bounding_box['x_pos'], bounding_box['y_pos'], bounding_box['width'], bounding_box['height'], Qt.white)
-    #
-    #     self.update()
-
     def on_new_tuio_bundle(self, data):
-        tokens = data['tokens']
         bounding_boxes = data['bounding_boxes']
 
-        self.reset_image()
+        now = int(round(time.time() * 1000))
+
+        MAX_TIME_MISSING_MS = 1000
+        SMOOTHING_FACTOR = 0.7  # Value between 0 and 1, depending if the old or the new value should count more.
+
         for bounding_box in bounding_boxes:
-            print(bounding_box)
             if bounding_box['session_id'] == 10000 or bounding_box['session_id'] == 10001 or bounding_box['session_id'] == 10002:
                 entry = list(filter(lambda entry: entry['id'] == bounding_box['session_id'], self.nutri_scores))[0]
-                entry['x'] = bounding_box['x_pos']
-                entry['y'] = bounding_box['y_pos']
-                entry['width'] = bounding_box['width']
-                entry['height'] = bounding_box['height']
-                entry['last_time_seen'] = time.time()
+
+                entry['x'] = int(SMOOTHING_FACTOR * (bounding_box['x_pos'] - entry['x']) + entry['x'])
+                entry['y'] = int(SMOOTHING_FACTOR * (bounding_box['y_pos'] - entry['y']) + entry['y'])
+                entry['width'] = int(SMOOTHING_FACTOR * (bounding_box['width'] - entry['width']) + entry['width'])
+                entry['height'] = int(SMOOTHING_FACTOR * (bounding_box['height'] - entry['height']) + entry['height'])
+                entry['last_time_seen'] = int(round(time.time() * 1000))
 
                 if entry['hidden']:
                     entry['hidden'] = False
                     entry['widget'].show()
 
-                entry['widget'].move(entry['x'] + entry['width'], entry['y'])
-                self.draw_circle(entry['x'], entry['y'], entry['width'], entry['height'], Qt.white)
-
-                print(entry)
-
         self.update()
+        self.reset_image()
 
-        
+        for entry in self.nutri_scores:
+
+            if not entry['hidden']:
+                # Hide element if missing for too long
+                if now - entry['last_time_seen'] > MAX_TIME_MISSING_MS:
+                    entry['hidden'] = True
+                    entry['widget'].hide()
+                    break
+
+                # Draw elements:
+                self.draw_circle(entry['x'], entry['y'], entry['width'], entry['height'])
+
+                entry['widget'].move(entry['x'] + entry['width'], entry['y'])
+
 
 class NutriScore(QWidget):
 
@@ -168,7 +146,7 @@ class NutriScore(QWidget):
         self.x = x
         self.y = y
         self.item_name = item_name
-        self.calories_name = calories + ' Kalorien'
+        self.calories_name = 'Brennwert: ' + calories
         self.carbohydrates_name = carbohydrates + ' Kohlenhydrate'
         self.protein_name = protein + ' Eiweiß'
         self.fat_name = fat + ' Fett'
@@ -187,8 +165,6 @@ class NutriScore(QWidget):
         self.label_fat = self.findChild(QLabel, 'fat')
 
     def init_values(self):
-        print(self.calories)
-
         self.label_name.setText(self.item_name)
         self.label_calories.setText(self.calories_name)
         self.label_carbohydrates.setText(self.carbohydrates_name)
